a78240f248a1188974e6ad01e0afd5021d2fb284
Andrew Levy <levyandrew44@yahoo.com>
2014-09-14 21:33:09 -0400

    error fixing

--- public/js/chatCtrl.js
+++ public/js/chatCtrl.js
@@ -6,14 +6,14 @@ angular.module("chatRoomApp")
    $scope.msgs = msgs.data;
   });
 
-  $scope.addMsg = function (msg){
+  $scope.addMsg = function( (msg){
    chatSvc.addMsg ({
     author: msg.author,
     content: msg.content,
    }).then(function(){
     $location.path("/");
    })
-  };
+  });
 
 
 
